5d482b2379d79797009432f4ee96020417924852
Jeong-Ag <kjungu1105@gmail.com>
2024-06-09 23:38:45 +0900

    feat: navbar, footer 생성

---
+++ src/component/common/Navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 z-10 w-full py-3 text-center bg-white border-b text-main-point">
+      <p className="text-lg font-bold">슬로생 Slo-saeng</p>
+    </nav>
+  );
+};
+export default Navbar;
