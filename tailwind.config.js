acc9e1e7e40991f0c56cad4a129196dbdc87e34f
Jeong-Ag <kjungu1105@gmail.com>
2024-06-09 17:14:16 +0900

    feat: 메인 색상 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-point': '#FCBA69',
+        'main-base': '#FFF5E8',
+      },
+    },
   },
   plugins: [],
 };
