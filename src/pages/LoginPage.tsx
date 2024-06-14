d8439ed9d16ebb44bfbb60ee2ddd49c83be72ad4
sooin <csi44029674@gmail.com>
2024-06-14 17:20:24 +0900

    feat : id, pwd 한글 입력 금지 구현

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,28 +1,38 @@
 import React, { ChangeEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import Button from '../component/common/Button/Button';
 
 const LoginPage = () => {
   const [userId, setUserId] = useState('');
   const [userPwd, setUserPwd] = useState('');
+  const [showPwd, setShowPwd] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const handleUserId = (e: ChangeEvent<HTMLInputElement>) => {
-    setUserId(e.target.value);
-  };
+  const HANGUL_REGEX = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
 
-  const handleUserPwd = (e: ChangeEvent<HTMLInputElement>) => {
-    setUserPwd(e.target.value);
-  };
+  const handleInputChange =
+    (setter: React.Dispatch<React.SetStateAction<string>>) =>
+    (e: ChangeEvent<HTMLInputElement>) => {
+      if (!HANGUL_REGEX.test(e.target.value)) {
+        setter(e.target.value);
+        setErrorMessage('');
+      } else {
+        setErrorMessage('아이디와 비밀번호에 한글은 입력할 수 없습니다.');
+      }
+    };
 
   const handleLogin = () => {
     if (!userId || !userPwd) {
       setErrorMessage('아이디와 비밀번호를 모두 입력해주세요.');
       return;
     }
-    // 임시 콘솔 출력
     console.log('userId:', userId);
     console.log('userPwd:', userPwd);
+  };
+
+  const toggleShowPwd = () => {
+    setShowPwd(!showPwd);
   };
 
   return (
@@ -32,21 +42,30 @@ const LoginPage = () => {
           <p className="font-bold text-5xl pb-3">슬로생</p>
           <p className="text-2xl"> 슬기로운 노후 생활로 가는 한 걸음</p>
         </div>
-        <div className="">
+        <div>
           <input
             type="text"
             placeholder="아이디를 입력해주세요."
             className="block w-full px-4 py-4 border rounded"
-            onChange={handleUserId}
+            value={userId}
+            onChange={handleInputChange(setUserId)}
           />
         </div>
-        <div>
+        <div className="relative">
           <input
-            type="password"
+            type={showPwd ? 'text' : 'password'}
             placeholder="비밀번호를 입력해주세요."
             className="block w-full px-4 py-4 border rounded"
-            onChange={handleUserPwd}
+            value={userPwd}
+            onChange={handleInputChange(setUserPwd)}
           />
+          <button
+            type="button"
+            className="absolute right-4 top-5 text-gray-500 hover:text-gray-700"
+            onClick={toggleShowPwd}
+          >
+            {showPwd ? <FaEyeSlash /> : <FaEye />}
+          </button>
         </div>
         {errorMessage && <p className="text-red-500">{errorMessage}</p>}
         <div className="pt-5">
