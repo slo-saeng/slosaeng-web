b534d4b147c17605619672d2ff2b4353b6fe36aa
Jeong-Ag <kjungu1105@gmail.com>
2024-06-20 14:35:54 +0900

    feat: super 관리자 페이지 생성

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,6 +3,7 @@ import Layout from './Layout';
 import MainPage from '../pages/MainPage';
 import RegisterPage from '../pages/RegisterPage';
 import HospitalPage from '../pages/HospitalPage';
+import SuperPage from '../pages/SuperPage';
 
 const Router = () => {
   const router = createBrowserRouter([
@@ -21,6 +22,10 @@ const Router = () => {
         {
           path: '/hospital',
           element: <HospitalPage />,
+        },
+        {
+          path: '/super',
+          element: <SuperPage />,
         },
       ],
     },
