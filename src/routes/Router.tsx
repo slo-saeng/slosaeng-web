0b0b113cd5a42d551f39fa30f512a4c8d82be3c9
Jeong-Ag <kjungu1105@gmail.com>
2024-06-20 15:20:35 +0900

    refactor: import 누락 수정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -5,6 +5,7 @@ import SignUpPage from '../pages/SignUpPage';
 import RegisterPage from '../pages/RegisterPage';
 import HospitalPage from '../pages/HospitalPage';
 import LoginPage from '../pages/LoginPage';
+import SuperPage from '../pages/SuperPage';
 
 const Router = () => {
   const router = createBrowserRouter([
