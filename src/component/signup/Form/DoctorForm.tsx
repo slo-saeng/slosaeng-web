69e7e3509ace82b3030c82a6005c5d7fee946d3b
Jeong-Ag <kjungu1105@gmail.com>
2024-06-21 18:38:54 +0900

    refactor: 닥터 회원가입 시 api 요청에 필요한 값 추가 및 스타일 변경

--- src/component/signup/Form/DoctorForm.tsx
+++ src/component/signup/Form/DoctorForm.tsx
@@ -1,66 +1,77 @@
 import { ChangeEvent, useEffect, useState } from 'react';
 import Button from '../../common/Button/Button';
 import { PasswordInput } from '../PasswordInput/PasswordInput';
+import { useDoctorMutation } from '../../../hooks/useDoctorMutation';
+import { forbiddenKorean, hyphensPhoneNumber } from '../../../utils/privacy';
+import type { doctorProfile } from '../../../types/member';
 
 export const DoctorForm = () => {
-  const [formValues, setFormValues] = useState({
+  const [formValues, setFormValues] = useState<doctorProfile>({
     id: '',
     password: '',
-    ssnFront: '',
-    ssnBack: '',
-    contact: '',
-    hospitalName: '',
-    agreement: '',
-    agreed: false,
+    name: '',
+    phone: '',
+    position: '',
+    birth: '',
+    institutionNumber: '',
   });
-
+  const [agree, setAgree] = useState<boolean>(false);
   const [isValidId, setIsValidId] = useState(true);
   const [isValidPassword, setIsValidPassword] = useState(true);
   const [isFormComplete, setIsFormComplete] = useState(false);
+  const { doctorMutate } = useDoctorMutation();
 
   const handleChange = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
     const { name, value } = e.target;
-    const isValid = /^[^\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3]*$/.test(
-      value,
-    );
 
-    if (name === 'id') {
-      setIsValidId(isValid);
-    } else if (name === 'password') {
-      setIsValidPassword(isValid);
-    }
-
-    if (isValid || name === 'hospitalName') {
-      setFormValues((prevValues) => ({
-        ...prevValues,
-        [name]: value,
+    if (name === 'phone') {
+      setFormValues((prevInfo) => ({
+        ...prevInfo,
+        [name]: hyphensPhoneNumber(value),
       }));
+      return;
     }
+    if (name === 'id') {
+      setIsValidId(forbiddenKorean(value));
+    } else if (name === 'password') {
+      setIsValidPassword(forbiddenKorean(value));
+    }
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
   };
 
   useEffect(() => {
-    const { id, password, ssnFront, contact, hospitalName, agreed } =
+    const { id, password, phone, institutionNumber, name, position, birth } =
       formValues;
     const isValidForm =
       id !== '' &&
       password !== '' &&
-      ssnFront !== '' &&
-      contact !== '' &&
-      hospitalName !== '' &&
-      agreed;
+      phone !== '' &&
+      institutionNumber !== '' &&
+      name !== '' &&
+      position !== '' &&
+      birth !== '' &&
+      agree !== false;
     setIsFormComplete(isValidForm);
-  }, [formValues]);
-  const handleCheckboxChange = () => {
-    setFormValues((prevValues) => ({
-      ...prevValues,
-      agreed: !prevValues.agreed,
-    }));
-  };
+  }, [formValues, agree]);
 
   const handleSubmit = () => {
-    alert('회원가입이 완료되었습니다.');
+    const { id, password, phone, institutionNumber, name, position, birth } =
+      formValues;
+    if (isFormComplete)
+      doctorMutate({
+        id,
+        password,
+        name,
+        phone,
+        position,
+        birth,
+        institutionNumber,
+      });
   };
 
   return (
@@ -85,53 +96,60 @@ export const DoctorForm = () => {
       {!isValidPassword && (
         <p className="text-red-500">비밀번호에 한글은 입력할 수 없습니다.</p>
       )}
-      <div className="flex">
-        <input
-          type="number"
-          name="ssnFront"
-          value={formValues.ssnFront}
-          onChange={handleChange}
-          placeholder="주민번호"
-          className="block w-full px-4 py-4 mt-2 border rounded focus:outline-none"
-        />
-      </div>
       <input
-        type="number"
-        name="contact"
-        value={formValues.contact}
+        type="text"
+        name="name"
+        value={formValues.name}
+        onChange={handleChange}
+        placeholder="이름"
+        className="block w-full px-4 py-4 mt-2 border rounded focus:outline-none"
+      />
+      <input
+        name="phone"
+        value={formValues.phone}
         onChange={handleChange}
         placeholder="연락처"
         className="block w-full px-4 py-4 mt-2 border rounded focus:outline-none"
       />
       <input
-        type="text"
-        name="hospitalName"
-        value={formValues.hospitalName}
+        name="birth"
+        value={formValues.birth}
         onChange={handleChange}
-        placeholder="소속병원명"
+        placeholder="생년월일 ex)19991010"
         className="block w-full px-4 py-4 mt-2 border rounded focus:outline-none"
       />
-      <div className="py-4 mt-2">
-        <p>약관동의</p>
-        <textarea
-          name="agreement"
-          value={formValues.agreement}
-          onChange={handleChange}
-          placeholder="약관 동의 내용을 입력하세요."
-          rows={5}
-          className="block w-full px-3 py-2 mt-2 border rounded focus:border-blue-500 focus:outline-none"
-        />
-        <input
-          type="checkbox"
-          name="checked"
-          checked={formValues.agreed}
-          onChange={handleCheckboxChange}
-          className="mr-2"
-        />
+      <input
+        name="institutionNumber"
+        value={formValues.institutionNumber}
+        onChange={handleChange}
+        placeholder="소속병원코드"
+        className="block w-full px-4 py-4 mt-2 border rounded focus:outline-none"
+      />
+      <input
+        name="position"
+        value={formValues.position}
+        onChange={handleChange}
+        placeholder="직급"
+        className="block w-full px-4 py-4 mt-2 border rounded focus:outline-none"
+      />
+      <div className="py-4 mt-2 space-y-2">
+        <p className="block w-full h-40 px-3 py-2 mt-2 text-sm text-gray-400 border rounded">
+          약관 안내
+        </p>
+        <div className="flex">
+          <input
+            type="checkbox"
+            name="checked"
+            checked={agree}
+            onChange={() => setAgree(!agree)}
+            className="mr-2"
+          />
+          <p>약관에 동의합니다.</p>
+        </div>
       </div>
-      <div className="">
+      <div>
         {!isFormComplete && (
-          <p className="text-red-500">필수 입력사항들을 입력해주세요.</p>
+          <p className="mb-2 text-red-500">필수 입력사항들을 입력해주세요.</p>
         )}
         <Button
           className={`py-4 text-black w-full rounded-md focus:outline-none ${
