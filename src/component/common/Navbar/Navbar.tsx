7a016e2faabaab952bbf14614b53ae41996c6c79
Jeong-Ag <kjungu1105@gmail.com>
2024-06-22 17:22:18 +0900

    feat: navbar에 현재 로그인 한 사람 조회

--- src/component/common/Navbar/Navbar.tsx
+++ src/component/common/Navbar/Navbar.tsx
@@ -1,7 +1,20 @@
+import { useMember } from '../../../hooks/useMember';
+
 const Navbar = () => {
+  const { data } = useMember();
+
   return (
-    <nav className="fixed top-0 left-0 z-10 w-full py-3 text-center bg-white border-b text-main-point">
+    <nav className="fixed top-0 left-0 z-10 flex justify-between w-full px-12 py-3 text-center bg-white border-b text-main-point">
       <p className="text-lg font-bold">슬로생 Slo-saeng</p>
+      {data && (
+        <p className="text-black">
+          안녕하세요{' '}
+          <span className="font-extrabold text-main-point-dark">
+            {data?.data.name}
+          </span>
+          님!
+        </p>
+      )}
     </nav>
   );
 };
