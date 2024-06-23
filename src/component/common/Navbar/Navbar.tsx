44b38f1c1c9209914c2f460d52ec328868743ed5
Jeong-Ag <kjungu1105@gmail.com>
2024-06-23 20:34:56 +0900

    feat: 로그아웃 버튼 생성

--- src/component/common/Navbar/Navbar.tsx
+++ src/component/common/Navbar/Navbar.tsx
@@ -1,19 +1,31 @@
-import { useMember } from '../../../hooks/useMember';
+import { useEffect, useState } from 'react';
+import { CiLogout } from 'react-icons/ci';
 
 const Navbar = () => {
-  const { data } = useMember();
+  const [logout, setLogout] = useState<boolean>(
+    Boolean(localStorage.getItem('refreshToken')),
+  );
+
+  const onClickLogout = () => {
+    localStorage.clear();
+    alert('로그아웃 되었습니다.');
+    setLogout(false);
+  };
+
+  useEffect(() => {
+    const refreshToken = localStorage.getItem('refreshToken');
+    setLogout(Boolean(refreshToken));
+  }, [setLogout]);
 
   return (
-    <nav className="fixed top-0 left-0 z-10 flex justify-between w-full px-12 py-3 text-center bg-white border-b text-main-point">
+    <nav className="fixed top-0 left-0 z-10 flex justify-between w-full px-12 py-3 bg-white border-b text-main-point">
       <p className="text-lg font-bold">슬로생 Slo-saeng</p>
-      {data && (
-        <p className="text-black">
-          안녕하세요{' '}
-          <span className="font-extrabold text-main-point-dark">
-            {data?.data.name}
-          </span>
-          님!
-        </p>
+      {logout && (
+        <CiLogout
+          size="32"
+          onClick={() => onClickLogout()}
+          className="p-1 rounded-md hover:cursor-pointer hover:bg-gray-200"
+        />
       )}
     </nav>
   );
