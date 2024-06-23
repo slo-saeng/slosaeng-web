0d611f95b11d1d99e2ac64345b60e47021aaa033
Jeong-Ag <kjungu1105@gmail.com>
2024-06-23 20:35:31 +0900

    refactor: 로그인 api 수정

--- src/hooks/useLoginMutation.ts
+++ src/hooks/useLoginMutation.ts
@@ -1,14 +1,17 @@
-import { useNavigate } from 'react-router-dom';
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import { postLogin } from '../api/login';
 
 export const useLoginMutation = () => {
-  const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: postLogin,
     onSuccess: (data) => {
-      if (data) navigate('/');
-      else alert('아이디나 비밀번호를 확인해주세요.');
+      if (data) {
+        queryClient.invalidateQueries({
+          queryKey: ['login'],
+        });
+        window.location.href = '/';
+      } else alert('아이디나 비밀번호를 확인해주세요.');
     },
     onError: () => {
       alert('로그인에 실패했습니다.');
