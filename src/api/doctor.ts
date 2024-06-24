a67e41504f41f31144deeae9df4902248a931402
Jeong-Ag <kjungu1105@gmail.com>
2024-06-24 11:09:54 +0900

    fix: 의료진 목록 조회 오류 수정

--- src/api/doctor.ts
+++ src/api/doctor.ts
@@ -25,7 +25,7 @@ export const deleteCancelDoctor = async (doctorId: string) => {
 };
 
 export const getDoctor = async () => {
-  const response = await server.delete(`/doctor`);
+  const response = await server.get(`/doctor`);
   if (response.status !== 200) {
     throw new Error('의료진 조회에 실패했습니다.');
   }
