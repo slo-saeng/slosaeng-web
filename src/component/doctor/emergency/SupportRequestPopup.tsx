f6a2a242a5a2c6df17167acb18b5bbc9c3fb261d
Jeong-Ag <kjungu1105@gmail.com>
2024-06-25 21:06:12 +0900

    refactor: 긴급 도움 요청 팝업 타입 값 맞도록 수정

--- src/component/doctor/emergency/SupportRequestPopup.tsx
+++ src/component/doctor/emergency/SupportRequestPopup.tsx
@@ -1,80 +1,58 @@
 import React from 'react';
-
-type Grade = '관심' | '주의' | '심각';
+import type { emergencyRecieve } from '../../../types/emergency';
+import { useCancelEmergencyMutation } from '../../../hooks/useCancelEmergencyMutation';
 
 interface ElderlyData {
+  elderlyInfo: emergencyRecieve;
   onClose: () => void;
-  elderlyInfo: {
-    id: number;
-    name: string;
-    gender: string;
-    birth: string;
-    age: number;
-    residence: string;
-    bloodType: string;
-    otherInfo: string;
-    phone: string;
-    grade?: Grade;
-  };
-  help: {
-    requestHelp: string;
-  };
 }
 const SupportRequestPopup: React.FC<ElderlyData> = ({
   onClose,
-  help,
   elderlyInfo,
-}) => {
+}: ElderlyData) => {
+  const { cancelEmergencyMutate } = useCancelEmergencyMutation();
   const handleAccept = () => {
-    console.log('긴급 도움 요청을 수락하셨습니다.');
+    alert('긴급 도움 요청을 수락하셨습니다.');
     onClose();
   };
   const handleReject = () => {
-    console.log('긴급 도움 요청을 거절했습니다.');
+    cancelEmergencyMutate(elderlyInfo.id);
     onClose();
   };
   return (
-    <div className="fixed inset-0 bg-red-400 bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white p-6 rounded shadow-lg w-2/4 space-y-3">
-        <h2 className="text-3xl font-bold mb-4">긴급 도움 요청 정보</h2>
+    <div className="fixed inset-0 z-30 flex items-center justify-center bg-red-400 bg-opacity-50">
+      <div className="w-2/4 p-6 space-y-3 bg-white rounded shadow-lg">
+        <h2 className="mb-4 text-3xl font-bold">긴급 도움 요청 정보</h2>
         <p className="font-bold">도움 요청</p>
-        <p className="mb-3 border px-3 py-2">{help.requestHelp}</p>
+        <p className="px-3 py-2 mb-3 border">{elderlyInfo.elder.etc}</p>
         <p className="font-bold">고령자정보</p>
-        <table className="table-auto w-full mb-4 text-left">
+        <table className="w-full mb-4 text-left table-auto">
           <thead>
             <tr className="text-sm">
-              <th className="border px-3  py-2">이름 / 나이 / 성별</th>
-              <th className="border px-3  py-2">혈액형</th>
-              <th className="border px-3  py-2">주소</th>
-              <th className="border px-3  py-2">전화번호</th>
-              {elderlyInfo.grade && (
-                <th className="border px-3  py-2">주요관리 등급</th>
-              )}
-              {!elderlyInfo.grade && (
-                <th className="border px-3  py-2">주요관리 대상</th>
-              )}
+              <th className="px-3 py-2 border">이름 / 나이 / 성별</th>
+              <th className="px-3 py-2 border">혈액형</th>
+              <th className="px-3 py-2 border">주소</th>
+              <th className="px-3 py-2 border">전화번호</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td className="border px-3 py-2">
-                {elderlyInfo.name} / {elderlyInfo.age} / {elderlyInfo.gender}
+              <td className="px-3 py-2 border">
+                {elderlyInfo.elder.name} / {elderlyInfo.elder.idNumber} /{' '}
+                {elderlyInfo.elder.gender}
               </td>
 
-              <td className="border px-3 py-2">{elderlyInfo.bloodType}</td>
+              <td className="px-3 py-2 border">
+                {elderlyInfo.elder.bloodType}
+              </td>
 
-              <td className="border px-4 py-2">{elderlyInfo.residence}</td>
+              <td className="px-4 py-2 border">
+                {elderlyInfo.elder.nation.name} {elderlyInfo.elder.city.name}{' '}
+                {elderlyInfo.elder.detailAddress}{' '}
+                {elderlyInfo.elder.nation.name}
+              </td>
 
-              <td className="border px-4 py-2">{elderlyInfo.phone}</td>
-
-              {elderlyInfo.grade && (
-                <td className="border px-4 py-2 border-gray-300">
-                  {elderlyInfo.grade}
-                </td>
-              )}
-              {!elderlyInfo.grade && (
-                <td className="border px-4 py-2 border-gray-300">X</td>
-              )}
+              <td className="px-4 py-2 border">{elderlyInfo.elder.phone}</td>
             </tr>
           </tbody>
         </table>
@@ -82,7 +60,7 @@ const SupportRequestPopup: React.FC<ElderlyData> = ({
           <div className="pr-1">
             <button
               type="button"
-              className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition duration-300"
+              className="px-4 py-2 text-white transition duration-300 bg-green-500 rounded hover:bg-green-600"
               onClick={handleAccept}
             >
               수락
@@ -91,7 +69,7 @@ const SupportRequestPopup: React.FC<ElderlyData> = ({
           <div className="pl-1">
             <button
               type="button"
-              className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-300"
+              className="px-4 py-2 text-white transition duration-300 bg-red-500 rounded hover:bg-red-600"
               onClick={handleReject}
             >
               거절
